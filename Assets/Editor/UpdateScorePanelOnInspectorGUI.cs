372f028d275ca10fe024f701610087f4f99b46ef
Mojopon <tyamuro@gmail.com>
2016-06-21 10:40:05 +0900

    create ScorePanel
    create ScorePanel

---
+++ Assets/Editor/UpdateScorePanelOnInspectorGUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(ScorePanel))]
+public class UpdateScorePanelOnInspectorGUI : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var scorePanel = target as ScorePanel;
+        scorePanel.UpdateScore();
+    }
+}
---
+++ Assets/Scripts/UI/ScorePanel.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScorePanel : MonoBehaviour
+{
+    [SerializeField]
+    private int _Rank;
+    [SerializeField]
+    private Text _ScoreText;
+    [SerializeField]
+    private Text _PlayerNameText;
+
+    [SerializeField]
+    private int _score = 0;
+    [SerializeField]
+    private string _playerName = "";
+
+    public void SetScore(int rank, int score, string playerName)
+    {
+        _Rank = rank;
+        _score = score;
+        _playerName = playerName;
+        UpdateScore();
+    }
+
+    public void UpdateScore()
+    {
+        if(_ScoreText)
+            _ScoreText.text = FormattedScore;
+        
+        if(_PlayerNameText)
+            _PlayerNameText.text = _playerName;
+    }
+
+    private string FormattedScore { get { return _Rank.ToString() + ". " + _score.ToString(); } }
+}
