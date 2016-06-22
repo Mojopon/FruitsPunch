6108f3b46c652aa6e721672aad75c1e54cc81124
Mojopon <tyamuro@gmail.com>
2016-06-22 11:46:15 +0900

    moved InputManager's components to its own folder
    moved InputManager's components to its own folder

--- Assets/Scripts/UIResource.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
-
-public class UIResource : SingletonMonoBehaviour<UIResource>
-{
-    public Text timeLeftText;
-    public Text countdownText;
-}
