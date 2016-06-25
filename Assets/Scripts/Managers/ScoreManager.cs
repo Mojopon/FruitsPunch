41ea9bd15beb5c821dd004ea5241c92b858619c4
Mojopon <tyamuro@gmail.com>
2016-06-25 14:20:10 +0900

    add ScoreReactiveProperty to ScoreManager and DisplayCurrentScore to display it
    add ScoreReactiveProperty to ScoreManager and DisplayCurrentScore to
    display it

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -5,12 +5,29 @@ using System.Collections;
 
 public class ScoreManager : ReactiveSingletonMonoBehaviour<ScoreManager>, IObservableHighscore
 {
+    [SerializeField]
+    private int _ScoreGainPerFruit = 100;
+
     public IObservable<IList<HighscoreData>> HighScoreDataObservable { get { return _highscoreDataStream.AsObservable(); } }
     private BehaviorSubject<IList<HighscoreData>> _highscoreDataStream = new BehaviorSubject<IList<HighscoreData>>(null);
 
+    public ReadOnlyReactiveProperty<int> ScoreReactiveProperty { get { return _scoreReactiveProperty.ToReadOnlyReactiveProperty(); } }
+    private ReactiveProperty<int> _scoreReactiveProperty = new ReactiveProperty<int>(0);
+
     void Start()
     {
+        ResetScore();
         _highscoreDataStream.OnNext(GetHighscores());
+
+        GameState.Instance.GameStateReactiveProperty
+                          .Where(x => x == GameStateEnum.DiscardFruitsPunch)
+                          .Subscribe(x => ResetScore())
+                          .AddTo(gameObject);
+    }
+
+    void ResetScore()
+    {
+        _scoreReactiveProperty.Value = 0;
     }
 
     private IList<HighscoreData> GetHighscores()
---
+++ Assets/Scripts/UI/Scores/DisplayCurrentScore.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UniRx;
+using System;
+
+public class DisplayCurrentScore : MonoBehaviour
+{
+    void Start()
+    {
+        ScoreManager.ObservableInstance
+                    .Where(x => x != null)
+                    .Subscribe(x => SubscribeOnScoreManager(x))
+                    .AddTo(gameObject);
+
+    }
+
+    private IDisposable subscription;
+    void SubscribeOnScoreManager(ScoreManager instance)
+    {
+        subscription = instance.ScoreReactiveProperty
+                               .Subscribe(x => OnScoreChange(x))
+                               .AddTo(gameObject);
+    }
+
+    void OnScoreChange(int score)
+    {
+        GetComponent<Text>().text = score.ToString();
+    }
+}
