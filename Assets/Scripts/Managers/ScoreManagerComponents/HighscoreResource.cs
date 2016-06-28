9d26a326344620ac23e1930fbc3b0a5091deca0f
Mojopon <tyamuro@gmail.com>
2016-06-28 10:07:49 +0900

    add feature to Save Highscores to PlayerPrefs
    add feature to Save Highscores to PlayerPrefs

--- Assets/Scripts/Managers/ScoreManagerComponents/HighscoreResource.cs
+++ Assets/Scripts/Managers/ScoreManagerComponents/HighscoreResource.cs
@@ -8,26 +8,75 @@ namespace ScoreManagerComponents
 {
     public class HighscoreResource : MonoBehaviour, IHighscoreResource
     {
+        private readonly static string SCORE_PLAYERPREFS_KEY = "Score";
+        private string GetScoreKey(int id) { return SCORE_PLAYERPREFS_KEY + id.ToString(); }
+
+        private readonly static string COMBO_PLAYERPREFS_KEY = "Combo";
+        private string GetComboKey(int id) { return COMBO_PLAYERPREFS_KEY + id.ToString(); }
+
         #region Related to get Highscore
-        private IHighscores highscores = null;
+        private IHighscores _highscores = null;
 
         public IHighscores GetHighscores()
         {
-            if (highscores == null)
+            if (_highscores == null)
             {
-                highscores = new Highscores();
+                _highscores = new Highscores();
+                LoadHighscoresFromPlayerPrefs();
             }
 
-            return highscores;
+            return _highscores;
+        }
+
+        void LoadHighscoresFromPlayerPrefs()
+        {
+            for(int i = 0; i < _highscores.Count; i++)
+            {
+                LoadHighscoreTable(i);
+            }
+        }
+
+        void LoadHighscoreTable(int id)
+        {
+            var score = PlayerPrefs.GetInt(GetScoreKey(id));
+            var combo = PlayerPrefs.GetInt(GetComboKey(id));
+
+            _highscores.AddHighscore(HighscoreData.Create(score, combo));
         }
 
         #endregion
 
         #region Related to Add Highscore
 
+        // Add new score to Highscores class.
+        // then going to save the scores to player prefs
         public bool AddHighscore(HighscoreData newScore)
         {
-            return highscores.AddHighscore(newScore);
+            if(_highscores.AddHighscore(newScore))
+            {
+                SaveHighscoresToPlayerPrefs();
+                return true;
+            }
+
+            return false;
+        }
+
+        void SaveHighscoresToPlayerPrefs()
+        {
+            for(int i = 0; i < _highscores.Count; i++)
+            {
+                SaveHighscoreTable(i);
+            }
+
+            PlayerPrefs.Save();
+        }
+
+        void SaveHighscoreTable(int id)
+        {
+            var highscoreTable = _highscores[id];
+
+            PlayerPrefs.SetInt(GetScoreKey(id), highscoreTable.score);
+            PlayerPrefs.SetInt(GetComboKey(id), highscoreTable.combo);
         }
 
         #endregion
