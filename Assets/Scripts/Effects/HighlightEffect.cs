3c8c0d5f65ad7f47ce9311aa99e02c57f9974df0
Mojopon <tyamuro@gmail.com>
2016-07-03 22:26:33 +0900

    create HighlightEffect class to control Highlight Object
    create HighlightEffect class to control Highlight Object

---
+++ Assets/Scripts/Effects/HighlightEffect.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using FruitsPunchInGameScripts;
+
+public class HighlightEffect : MonoBehaviour
+{
+    public float timeToFadeOut = 1f;
+
+    private SpriteRenderer sprite;
+
+    void Start ()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+
+        var fruitsPunchIngameProperties = FruitsPunchManager.Instance as IFruitsPunchInGameProperties;
+        if (fruitsPunchIngameProperties == null)
+        {
+            Debug.LogError("Couldnt find FruitsPunchManager");
+        }
+        else
+        {
+            radius = fruitsPunchIngameProperties.FruitsDeleteRadius;
+        }
+
+        transform.localScale = new Vector3(radius, radius);
+        StartCoroutine(SequenceFadeOut());
+	}
+
+    private float radius = 1f;
+    private float progress = 0;
+    IEnumerator SequenceFadeOut()
+    {
+        progress = 0;
+
+        while(1 > progress)
+        {
+            progress += Time.deltaTime / timeToFadeOut;
+
+            var color = sprite.color;
+            color.a = 1 - progress;
+
+            sprite.color = color;
+
+            yield return null;
+        }
+
+        Destroy(gameObject, 1f);
+        yield break;
+    }
+}
